aed4057ae457bba804fcee2641a1d4ea2abf2469
Chunyang Mou <cmou@usc.edu>
2020-03-28 18:21:28 -0700

    wrappingColumn

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@
 import 'package:eat_where/pages/register_page.dart';
 import 'package:flutter/material.dart';
 import '../utils/color_util.dart';
+import 'package:flutter/cupertino.dart';
 
 /*
   Login page.
@@ -14,9 +15,21 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextStyle textStyle = TextStyle(fontSize: 20.0);
 
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _username;
+
   @override
   Widget build(BuildContext context) {
-    final usernameField = TextField(
+    final usernameField = TextFormField(
+      onSaved: (value) => _username = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter some text";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -26,7 +39,14 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
 
-    final passwordField = TextField(
+    final passwordField = TextFormField(
+      onSaved: (value) => _password = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter some text";
+        }
+        return null;
+      },
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -42,7 +62,15 @@ class _LoginPageState extends State<LoginPage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          final form = _formKey.currentState;
+          form.save();
+
+          if (form.validate()) {
+            print('username: $_username, password: $_password');
+            //TODO -> GO TO FIREBASE AND TRAVERSE TO ANOTHER PAGE
+          }
+        },
         child: Text("Login",
             textAlign: TextAlign.center,
             style: textStyle.copyWith(
@@ -72,33 +100,38 @@ class _LoginPageState extends State<LoginPage> {
         body: Center(
           child: Container(
             width: MediaQuery.of(context).size.width * 0.9,
-            color: Colors.white,
+            //color: Colors.white,
             child: Padding(
               padding: const EdgeInsets.all(36.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 145.0,
-                    child: Image.asset(
-                      "assets/images/logo.png", //add logo.png to the page
-                      fit: BoxFit.contain,
-                    ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 145.0,
+                        child: Image.asset(
+                          "assets/images/logo.png", //add logo.png to the page
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(height: 55.0),
+                      usernameField,
+                      SizedBox(height: 25.0),
+                      passwordField,
+                      SizedBox(
+                        height: 35.0,
+                      ),
+                      loginButton,
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      signUpButton
+                    ],
                   ),
-                  SizedBox(height: 55.0),
-                  usernameField,
-                  SizedBox(height: 25.0),
-                  passwordField,
-                  SizedBox(
-                    height: 35.0,
-                  ),
-                  loginButton,
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  signUpButton
-                ],
+                ),
               ),
             ),
           ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,13 +1,28 @@
 import '../utils/color_util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
 
-class RegisterPage extends StatelessWidget {
+class _RegisterPageState extends State<RegisterPage> {
   TextStyle textStyle = TextStyle(fontSize: 20.0);
-
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _confirmPassword;
+  String _username;
   @override
   Widget build(BuildContext context) {
-    final usernameField = TextField(
+    final usernameField = TextFormField(
+      onSaved: (value) => _username = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter some text";
+        }
+        return null;
+      },
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -16,8 +31,15 @@ class RegisterPage extends StatelessWidget {
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     );
-    
-    final passwordField = TextField(
+
+    final passwordField = TextFormField(
+      onSaved: (value) => _password = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter some text";
+        }
+        return null;
+      },
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -27,7 +49,16 @@ class RegisterPage extends StatelessWidget {
       ),
     );
 
-    final confirmPassField = TextField(
+    final confirmPassField = TextFormField(
+      onSaved: (value) => _confirmPassword = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Please enter some text";
+        } else if (value != _password) {
+          return "Passwords should match";
+        }
+        return null;
+      },
       obscureText: false,
       style: textStyle,
       decoration: InputDecoration(
@@ -36,7 +67,7 @@ class RegisterPage extends StatelessWidget {
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     );
-    
+
     final signUpButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -46,6 +77,13 @@ class RegisterPage extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
           //Get the result and deliver it to the back end
+          final form = _formKey.currentState;
+          form.save();
+
+          if (form.validate()) {
+            print('username: $_username, password: $_password');
+            //TODO -> GO TO FIREBASE AND TRAVERSE TO ANOTHER PAGE
+          }
         },
         child: Text("SignUp",
             textAlign: TextAlign.center,
@@ -64,31 +102,38 @@ class RegisterPage extends StatelessWidget {
             color: Colors.white,
             child: Padding(
               padding: const EdgeInsets.all(36.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 155.0,
-                    child: Image.asset(
-                      "assets/images/logo.png", //add logo.png to the page
-                      fit: BoxFit.contain,
+              child: SingleChildScrollView(
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 155.0,
+                          child: Image.asset(
+                            "assets/images/logo.png", //add logo.png to the page
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        SizedBox(height: 45.0),
+                        usernameField,
+                        SizedBox(height: 25.0),
+                        passwordField,
+                        SizedBox(height: 25.0),
+                        confirmPassField,
+                        SizedBox(
+                          height: 35.0,
+                        ),
+                        signUpButton,
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 45.0),
-                  usernameField,
-                  SizedBox(height: 25.0),
-                  passwordField,
-                  SizedBox(height: 25.0),
-                  confirmPassField,
-                  SizedBox(
-                    height: 35.0,
-                  ),
-                  signUpButton,
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                ],
+                ),
               ),
             ),
           ),
