dd9c8cf439ef6e69b94786f86759d82b3a7bbc32
LihanZhuH <lihanzhu@usc.edu>
2020-03-28 17:32:25 -0700

    Update group_page.dart

--- lib/pages/group_page.dart
+++ lib/pages/group_page.dart
@@ -20,7 +20,8 @@ class GroupPage extends StatefulWidget {
 }
 
 class _GroupPageState extends State<GroupPage> {
-  final _groups = <String>["Friday party", "Office lunch"];
+//  final _groups = <String>["Friday party", "Office lunch"];
+  List<Map<String, dynamic>> _groups = [];
 
   @override
   void initState() {
@@ -28,8 +29,12 @@ class _GroupPageState extends State<GroupPage> {
     Firestore.instance
         .collection("users")
         .document(widget.currentUserId)
-        .get().then((value) {
-          value.data
+        .get().then((document) {
+          var map = document.data;
+          var groupsArray = map['groups'];
+          setState(() {
+            _groups = groupsArray;
+          });
     });
   }
 
@@ -48,48 +53,30 @@ class _GroupPageState extends State<GroupPage> {
           }));
         },
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance
-            .collection('users')
-            .document(widget.currentUserId)
-            .collection('groups')
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return new Text('Loading...');
-          return new ListView(
-            children: snapshot.data.documents.map((DocumentSnapshot document) {
-              return new ListTile(
-                title: new Text(document['id']),
-                subtitle: new Text(document['owner'] ? "Owner" : "Non - owner"),
-              );
-            }).toList(),
-          );
-        },
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: _groups.length,
+          itemBuilder: (BuildContext ctx, int index) {
+            return InkWell(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(5),
+                height: 80,
+                decoration: BoxDecoration(
+                  border: Border.all(color: ColorUtils.borderColor),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Text(_groups[index]['name']),
+              ),
+              onTap: () {
+                print(index);
+                // TODO: page transition
+              },
+            );
+          },
+        ),
       )
-//      body: Container(
-//        margin: EdgeInsets.all(20),
-//        child: ListView.builder(
-//          itemCount: _groups.length,
-//          itemBuilder: (BuildContext ctx, int index) {
-//            return InkWell(
-//              child: Container(
-//                padding: EdgeInsets.all(20),
-//                margin: EdgeInsets.all(5),
-//                height: 80,
-//                decoration: BoxDecoration(
-//                  border: Border.all(color: ColorUtils.borderColor),
-//                  borderRadius: BorderRadius.all(Radius.circular(10)),
-//                ),
-//                child: Text(_groups[index]),
-//              ),
-//              onTap: () {
-//                print(index);
-//                // TODO: page transition
-//              },
-//            );
-//          },
-//        ),
-//      )
     );
   }
 }
